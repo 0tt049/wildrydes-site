8cc5d76861fe7283b2f964cdd75c4383f3ddf4ed
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-04-26 18:47:15 -0300

    Updated Invoke URL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'sa-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ta9apepfyc.execute-api.sa-east-1.amazonaws.com/prod'
     }
 };
